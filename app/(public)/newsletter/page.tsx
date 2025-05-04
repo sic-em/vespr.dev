7957e94df685aae86b0dd7a06b65bc19c3d51f74
Jawad <chaptrhouse@gmail.com>
2025-05-03 21:48:18 -0500

    refactor(app): refactor public and settings pages

--- app/(public)/newsletter/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function NewsletterPage() {
-	return (
-		<div className="flex min-h-screen flex-col items-center justify-start px-4 sm:px-6 pt-6 pb-16">
-			NewsLetterPage
-		</div>
-	);
-}
